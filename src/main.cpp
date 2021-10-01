c288adaddc98c53eb9c8cc6ca473084ac776e0d2
Junaid <junaid1460@gmail.com>
2021-10-01 17:28:32 +0530

    feat: baby step towards reading file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 
 #include <iostream>
 #include <defmacro/defmacro.h>
+#include <fstream>
 
 int main()
 {
+    std::ifstream ifs("test.cx");
+    std::string content((std::istreambuf_iterator<char>(ifs)),
+                        (std::istreambuf_iterator<char>()));
+
+    std::cout << content;
+    ifs.close();
     return 0;
 }
