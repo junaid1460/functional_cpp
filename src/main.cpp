acd4486bef94b6034df22d53e6b9e2509634a21d
Junaid <junaid1460@gmail.com>
2021-10-03 14:57:21 +0530

    Feat: add parsing expression grammar lib

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <defmacro/defmacro.h>
 #include <fstream>
+#include <peglib.h>
 
 int main()
 {
@@ -9,7 +10,23 @@ int main()
     std::string content((std::istreambuf_iterator<char>(ifs)),
                         (std::istreambuf_iterator<char>()));
 
+    peg::parser parser(R"(
+        ROOT        <- CONTENT
+        CONTENT     <- MACRO | ''
+        MACRO       <- 'defmacro' __ NAME __ MACRO_HEADER _ MACRO_HEADER
+        MACRO_HEADER  <- '(' OPTIONAL_PARAMS ')' | _
+        MACRO_BODY      <- 'do' < .* > 'end' CONTENT
+        OPTIONAL_PARAMS <- NAME | NAME _ ',' _ OPTIONAL_PARAMS
+        NAME        <- < [a-zA-Z_][[a-zA-Z0-9\?\!_] >
+        _  <-  [ \t\r\n]*
+        __  <-  [ \t\r\n]+
+
+    )");
+
     std::cout << content;
+
+    std::cout << parser.parse(content);
+
     ifs.close();
     return 0;
 }
